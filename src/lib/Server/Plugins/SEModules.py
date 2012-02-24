febab88690cc6a336c9b4e4b117409fe299ab66f
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-02-24 13:40:17 -0500

    made SEModules plugin work

--- src/lib/Server/Plugins/SEModules.py
+++ src/lib/Server/Plugins/SEModules.py
@@ -1,27 +1,57 @@
+import os
 import sys
-import binascii
 import logging
+import binascii
+import posixpath
 import lxml.etree
 
 import Bcfg2.Server.Plugin
 logger = logging.getLogger(__name__)
 
-class SEModuleEntrySet(Bcfg2.Server.Plugin.EntrySet):
-    def bind_entry(self, entry, metadata):
-        Bcfg2.Server.Plugin.EntrySet.bind_entry(self, entry, metadata)
+class SEModuleData(Bcfg2.Server.Plugin.SpecificData):
+    def bind_entry(self, entry, _):
         entry.set('encoding', 'base64')
-        entry.text = binascii.b2a_base64(entry.text)
+        entry.text = binascii.b2a_base64(self.data)
 
 class SEModules(Bcfg2.Server.Plugin.GroupSpool):
     """ Handle SEModule entries """
     name = 'SEModules'
     __author__ = 'chris.a.st.pierre@gmail.com'
-    es_cls = SEModuleEntrySet
-    es_child_cls = Bcfg2.Server.Plugin.SpecificData
+    es_cls = Bcfg2.Server.Plugin.EntrySet
+    es_child_cls = SEModuleData
     entry_type = 'SEModule'
+    experimental = True
 
     def HandlesEntry(self, entry, metadata):
         if entry.tag in self.Entries and entry.get("name").endswith(".pp"):
             return entry.get("name")[0:-3] in self.Entries[entry.tag]
         return Bcfg2.Server.Plugin.GroupSpool.HandlesEntry(self, entry,
                                                            metadata)
+
+    def add_entry(self, event):
+        epath = self.event_path(event)
+        ident = self.event_id(event)
+        if posixpath.isdir(epath):
+            self.AddDirectoryMonitor(epath[len(self.data):])
+        if ident not in self.entries and posixpath.isfile(epath):
+            dirpath = "/".join([self.data, ident])
+            basename = os.path.split(self.handles[event.requestID][:-1])[1]
+            print "basename=%s" % basename
+            self.entries[ident] = self.es_cls(basename,
+                                              dirpath,
+                                              self.es_child_cls,
+                                              self.encoding)
+            self.Entries[self.entry_type][ident] = \
+                self.entries[ident].bind_entry
+        if not posixpath.isdir(epath):
+            # do not pass through directory events
+            self.entries[ident].handle_event(event)
+
+    def event_id(self, event):
+        epath = self.event_path(event)
+        if posixpath.isdir(epath):
+            return Bcfg2.Server.Plugin.GroupSpool.event_id(self, event)
+        else:
+            # strip leading and trailing slashes
+            return self.handles[event.requestID][1:-1]
+        
